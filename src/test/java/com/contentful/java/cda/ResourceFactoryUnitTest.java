4c39e8c5a42c7499dffcc61b0d3fcff16a8a3809
Mario Bodemann <mario@contentful.com>
2016-07-08 11:14:53 +0200

    test preparation: inject constructor
    prepare a constructor to be used in unit tests to inject
    classes beeing static, or created in the default cstr.

---
+++ src/test/java/com/contentful/java/cda/ResourceFactoryUnitTest.java
@@ -0,0 +1,13 @@
+package com.contentful.java.cda;
+
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ResourceFactoryUnitTest {
+
+  public void testPreserveOrderOfElements() throws Exception {
+    ResourceFactory.array();
+  }
+
+}
