b39b31272153c0ea8dd80c0501c65446a8d4c0ba
= <dominik.lechl@alturos.com>
2020-07-14 09:44:34 +0200

    Add resource faker

---
+++ src/main/java/com/contentful/java/cda/CDAResourceFake.java
@@ -0,0 +1,37 @@
+package com.contentful.java.cda;
+
+import io.reactivex.annotations.NonNull;
+import io.reactivex.annotations.Nullable;
+
+public class CDAResourceFake extends CDAResource {
+
+    @Nullable
+    CDAResource getFake(
+            @NonNull String id,
+            @NonNull String contentTypeId,
+            @NonNull CDAType cdaType
+    ) {
+
+        if(cdaType == CDAType.ENTRY) {
+            CDAEntry entry = new CDAEntry();
+            entry.attrs.put("id", id);
+
+            CDAContentType contentType = new CDAContentType();
+            contentType.attrs.put("id", contentTypeId);
+
+            entry.setContentType(contentType);
+
+            return entry;
+        }
+        else if(cdaType == CDAType.ASSET) {
+            CDAAsset asset = new CDAAsset();
+
+            asset.attrs.put("id", id);
+
+            return asset;
+        }
+        else {
+            return null;
+        }
+    }
+}
