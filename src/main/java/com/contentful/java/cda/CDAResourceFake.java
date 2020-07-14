fa0d2d9c11d4538d2fe79d50199814d5973a5ed0
= <dominik.lechl@alturos.com>
2020-07-14 10:07:35 +0200

    Fix faked resource not set up correctly

--- src/main/java/com/contentful/java/cda/CDAResourceFake.java
+++ src/main/java/com/contentful/java/cda/CDAResourceFake.java
@@ -3,6 +3,10 @@ package com.contentful.java.cda;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CDAResourceFake extends CDAResource {
     private static final long serialVersionUID = 8340118714770272388L;
 
@@ -12,21 +16,27 @@ public class CDAResourceFake extends CDAResource {
             @NonNull String contentTypeId,
             @NonNull CDAType cdaType
     ) {
+        if (cdaType == CDAType.ENTRY) {
+            final CDAEntry entry = new CDAEntry();
+            final CDAContentType contentType = new CDAContentType();
 
-        if(cdaType == CDAType.ENTRY) {
-            CDAEntry entry = new CDAEntry();
+            entry.fields = new HashMap<>();
+            entry.attrs = new HashMap<>();
+            entry.attrs.put("type", cdaType.name());
             entry.attrs.put("id", id);
 
-            CDAContentType contentType = new CDAContentType();
+            contentType.fields = new ArrayList<>();
+            contentType.attrs = new HashMap<>();
             contentType.attrs.put("id", contentTypeId);
 
             entry.setContentType(contentType);
 
             return entry;
         }
-        else if(cdaType == CDAType.ASSET) {
-            CDAAsset asset = new CDAAsset();
+        else if (cdaType == CDAType.ASSET) {
+            final CDAAsset asset = new CDAAsset();
 
+            asset.attrs = new HashMap<>();
             asset.attrs.put("id", id);
 
             return asset;
