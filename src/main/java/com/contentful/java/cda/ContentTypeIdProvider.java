2a3bbefa59243ffe4ae25a53fdb7d27d0a3087f6
= <dominik.lechl@alturos.com>
2020-07-14 09:48:38 +0200

    Add nullable/nonnullable annotations to content type id provider

--- src/main/java/com/contentful/java/cda/ContentTypeIdProvider.java
+++ src/main/java/com/contentful/java/cda/ContentTypeIdProvider.java
@@ -1,5 +1,8 @@
 package com.contentful.java.cda;
 
+import io.reactivex.annotations.NonNull;
+import io.reactivex.annotations.Nullable;
+
 public interface ContentTypeIdProvider {
-    String getContentTypeId(String contentId);
+    @Nullable String getContentTypeId(@NonNull String contentId);
 }
