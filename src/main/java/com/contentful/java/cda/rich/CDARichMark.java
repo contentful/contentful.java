dfaceacadb84b6991f5cdaeec1eb891ef7ac8d9b
Mohamed Houssein Douici <douici1@gmail.com>
2022-10-27 11:39:26 +0200

    fix: add type property for rich mark classes besides custom mark (#269)
    * fix: add type property for rich mark classes besides custom mark
    
    * chore: dump version from 10.5.8 to 10.5.9
    
    Co-authored-by: Mohamed Houssein Douici <mohamed-houssein.douici@adesso.de>

--- src/main/java/com/contentful/java/cda/rich/CDARichMark.java
+++ src/main/java/com/contentful/java/cda/rich/CDARichMark.java
@@ -6,50 +6,75 @@ package com.contentful.java.cda.rich;
  * Subclasses are used for further differentiation.
  */
 public class CDARichMark {
+
+  public CDARichMark(String type) {
+    this.type = type;
+  }
+
+  /**
+   * @return the type of the marker.
+   */
+  public String getType() {
+    return type;
+  }
+
+  protected final String type;
+
   /**
    * A bold mark of a rich text.
    */
   public static class CDARichMarkBold extends CDARichMark {
+
+    public CDARichMarkBold() {
+      super("bold");
+    }
+
   }
 
   /**
    * Declares the text as being displayed in italics.
    */
   public static class CDARichMarkItalic extends CDARichMark {
+
+    public CDARichMarkItalic() {
+      super("italic");
+    }
+
   }
 
   /**
    * Marker for making the rich text displayed as underline.
    */
   public static class CDARichMarkUnderline extends CDARichMark {
+
+    public CDARichMarkUnderline() {
+      super("underline");
+    }
+
   }
 
   /**
    * The text marked by this marker should be represented by Code.
    */
   public static class CDARichMarkCode extends CDARichMark {
+
+    public CDARichMarkCode() {
+      super("code");
+    }
+
   }
 
   /**
    * Any custom marker for a given rich text.
    */
   public static class CDARichMarkCustom extends CDARichMark {
-    private final String type;
-
     /**
      * Create a custom marker using the given type.
      *
      * @param type which type should this marker have?
      */
     public CDARichMarkCustom(String type) {
-      this.type = type;
-    }
-
-    /**
-     * @return the custom type of the marker.
-     */
-    public String getType() {
-      return type;
+      super(type);
     }
   }
 }
