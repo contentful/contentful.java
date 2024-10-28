15468c70cc722e550eccc8668a8f5634e9830424
zeatful <zeatful@users.noreply.github.com>
2024-10-28 09:01:20 -0400

    feat: Implement Serializable interface for rich text classes (#308)
    The changes in this commit add the Serializable interface to various classes in the `com.contentful.java.cda.rich` package. This allows these classes to be easily serialized and deserialized, which is important for use cases where the rich text data needs to be persisted or transmitted over a network.
    
    The main classes that now implement Serializable are:
    
    - `CDARichTableCell`
    - `CDARichQuote`
    - `CDARichListItem`
    - `CDARichTableRow`
    - `CDARichTable`
    - `CDARichText`
    - `CDARichNode`
    - `CDARichMark`
    - `CDARichHorizontalRule`
    - `CDARichList`
    - `CDARichUnorderedList`
    - `CDARichEmbeddedInline`
    - `CDARichHyperLink`
    
    This change will enable better integration with other systems and frameworks that require serializable data, and will improve the overall flexibility and usability of the rich text handling functionality in the Contentful Java CDA SDK.
    
    Co-authored-by: Eric Foster <eric.foster@angi.com>
    Co-authored-by: Rafal Niski <rafalniski@gmail.com>

--- src/main/java/com/contentful/java/cda/rich/CDARichUnorderedList.java
+++ src/main/java/com/contentful/java/cda/rich/CDARichUnorderedList.java
@@ -1,9 +1,11 @@
 package com.contentful.java.cda.rich;
 
+import java.io.Serializable;
+
 /**
  * Representation of a block of unordered items.
  */
-public class CDARichUnorderedList extends CDARichList {
+public class CDARichUnorderedList extends CDARichList implements Serializable {
   /**
    * Create a list with bullet points.
    */
