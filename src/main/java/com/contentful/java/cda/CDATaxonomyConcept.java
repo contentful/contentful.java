3afe9fd2e16ce2859512dda9471c521960db11a8
Lisa Yang <lisa.yang.developer@gmail.com>
2025-09-17 14:12:32 +1000

    Add attributes

--- src/main/java/com/contentful/java/cda/CDATaxonomyConcept.java
+++ src/main/java/com/contentful/java/cda/CDATaxonomyConcept.java
@@ -1,11 +1,36 @@
 package com.contentful.java.cda;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Map;
 
 public class CDATaxonomyConcept extends CDAResource {
+    private static final long serialVersionUID = -2852530837647669036L;
+
+    @SerializedName("prefLabel")
+    protected Map<String, String> prefLabel;
+
+    /**
+     * @return the preferred label map containing locale-to-label mappings.
+     */
+    public Map<String, String> prefLabel() {
+        return prefLabel;
+    }
+
+    /**
+     * Get the preferred label for a specific locale.
+     * @param locale the locale code (e.g., "en-US")
+     * @return the label for the specified locale, or null if not found
+     */
+    public String getPrefLabel(String locale) {
+        return prefLabel != null ? prefLabel.get(locale) : null;
+    }
+
     @Override
     public String toString() {
         return "CDATaxonomyConcept{"
-                + "attrs=" + attrs
+                + "attrs=" + attrs + '\''
+                + ", prefLabel=" + prefLabel
                 + '}';
     }
 }
